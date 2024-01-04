cfe1bde41d00d1c5aed44e430207ba5a6228298e
digitalbricks <mail@andreherdling.de>
2024-01-04 08:36:49 +0100

    moved event-listener-adding to function for later re-use

--- src/asyncform.js
+++ src/asyncform.js
@@ -1,83 +1,94 @@
 /**
  * Asynchronous form submission with vanilla js.
- * @version: 1.0
+ * @version: 1.2
  * @credit: https://gomakethings.com/serializing-form-data-with-the-vanilla-js-formdata-object/  
  */
 
+bootstrapAsyncForm();
 
-// get all forms with class .async-form
-var forms = document.getElementsByClassName('async-form');
 
-// iterate of each form
-Array.prototype.forEach.call(forms, function(form) {
-    // get url from form action
-    var url = form.action;
 
-    // get element for server response messages
-    var messages = form.querySelector(".async-form__messages");
+/**
+ * Function for attaching event listeners to forms.
+ * This was moved inside a function to allow for
+ * later re-initialization of the event listeners – 
+ * e.g. when cloning a form via js.
+ */
+function bootstrapAsyncForm(){
+    // get all forms with class .async-form
+    var forms = document.getElementsByClassName('async-form');
 
-    // get submit button
-    var button = form.querySelector("button");
+    // iterate of each form
+    Array.prototype.forEach.call(forms, function(form) {
+        // get url from form action
+        var url = form.action;
+
+        // get element for server response messages
+        var messages = form.querySelector(".async-form__messages");
+
+        // get submit button
+        var button = form.querySelector("button");
     
 
-    // add a hidden input for indication the server
-    // that js was enabled (not necessary but you may react on)
-    var hidden_input = document.createElement("input");
-    hidden_input.type = "hidden";
-    hidden_input.name = "hasjs";
-    hidden_input.value = "true";
-    form.appendChild(hidden_input);
+        // add a hidden input for indication the server
+        // that js was enabled (not necessary but you may react on)
+        var hidden_input = document.createElement("input");
+        hidden_input.type = "hidden";
+        hidden_input.name = "hasjs";
+        hidden_input.value = "true";
+        form.appendChild(hidden_input);
 
 
-    // add event listener for form submit event
-    form.addEventListener('submit', function (event) {
+        // add event listener for form submit event
+        form.addEventListener('submit', function (event) {
 
-        // prevent form from submitting to the server
-        event.preventDefault();
+            // prevent form from submitting to the server
+            event.preventDefault();
 
-        // add class to button (for styling)
-        // and set to disabled (while fetching)
-        button.classList.add('async-form__button--fetching');
-        //button.disabled = true; // may harm accessibility, so disabled for now
+            // add class to button (for styling)
+            // and set to disabled (while fetching)
+            button.classList.add('async-form__button--fetching');
+            //button.disabled = true; // may harm accessibility, so disabled for now
         
-        // submit form via js
-        fetch(url, {
-            method: 'POST',
-            body: new FormData(event.target),
-        }).then(function (response) {
-            // The API call was successful!
+            // submit form via js
+            fetch(url, {
+                method: 'POST',
+                body: new FormData(event.target),
+            }).then(function (response) {
+                // The API call was successful!
             
-            // reset button
-            button.classList.remove('async-form__button--fetching');
-            //button.disabled = false; // may harm accessibility, so disabled for now
+                // reset button
+                button.classList.remove('async-form__button--fetching');
+                //button.disabled = false; // may harm accessibility, so disabled for now
             
-            // now we check the HTTP status
-            if (response.ok) {
-                // if ok we return the text from response
-                return response.text();
-            }
+                // now we check the HTTP status
+                if (response.ok) {
+                    // if ok we return the text from response
+                    return response.text();
+                }
 
-            // if not ok, we reject
-            return Promise.reject(response);
-        }).then(function (html) {
-            // update the form messages
-            messages.innerHTML = "<div class='async-form__message async-form__message--success'>" + html + "</div>";
+                // if not ok, we reject
+                return Promise.reject(response);
+            }).then(function (html) {
+                // update the form messages
+                messages.innerHTML = "<div class='async-form__message async-form__message--success'>" + html + "</div>";
 
-            // reset the form
-            form.reset();
-        }).catch(function (err) {
-            // There was an error
-            console.warn('Something went wrong.', err);
+                // reset the form
+                form.reset();
+            }).catch(function (err) {
+                // There was an error
+                console.warn('Something went wrong.', err);
 
-            // update form message (as err.text() is still a promise we have to use .then())
-            err.text().then(function(html){
-                messages.innerHTML = "<div class='async-form__message async-form__message--error'>" + html + "</div>";
-            })
+                // update form message (as err.text() is still a promise we have to use .then())
+                err.text().then(function(html){
+                    messages.innerHTML = "<div class='async-form__message async-form__message--error'>" + html + "</div>";
+                })
             
-        });
+            });
     
+        });
     });
-});
+}
 
 
 /**
